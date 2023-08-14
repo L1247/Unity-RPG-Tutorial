62d4e6787ab7a9ecdb9eea4d9a2fbb0673d9b952
r Star <starlove124760@gmail.com>
2023-08-15 04:13:09 +0800

    add PlayerCharacterData for binding moveSpeed

--- Assets/Game/Scripts/Players/Main/PlayerCharacterInstaller.cs
+++ Assets/Game/Scripts/Players/Main/PlayerCharacterInstaller.cs
@@ -1,7 +1,6 @@
 #region
 
 using Game.Scripts.Players.Handlers;
-using UnityEngine;
 using Zenject;
 
 #endregion
@@ -10,18 +9,10 @@ namespace Game.Scripts.Players.Main
 {
     public class PlayerCharacterInstaller : MonoInstaller
     {
-    #region Private Variables
-
-        [SerializeField]
-        private float moveSpeed = 5f;
-
-    #endregion
-
     #region Public Methods
 
         public override void InstallBindings()
         {
-            Container.Bind<float>().WithId("MoveSpeed").FromInstance(moveSpeed);
             Container.Bind<PlayerInputState>().AsSingle();
 
             Container.BindInterfacesTo<PlayerInputHandler>().AsSingle();
