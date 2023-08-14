aa7c9883e39fbc93b6ce5dd4c60aacd1792601dc
r Star <starlove124760@gmail.com>
2023-08-15 02:34:23 +0800

    add PlayerCharacter

---
+++ Assets/Game/Scripts/Players/Main/PlayerCharacter.cs
@@ -0,0 +1,10 @@
+#region
+
+using UnityEngine;
+
+#endregion
+
+namespace Game.Scripts.Players.Main
+{
+    public class PlayerCharacter : MonoBehaviour { }
+}
---
+++ Assets/Game/Scripts/Players/Main/PlayerCharacterInstaller.cs
@@ -0,0 +1,17 @@
+#region
+
+using Zenject;
+
+#endregion
+
+namespace Game.Scripts.Players.Main
+{
+    public class PlayerCharacterInstaller : MonoInstaller
+    {
+    #region Public Methods
+
+        public override void InstallBindings() { }
+
+    #endregion
+    }
+}
